cfc64305db28208e49993fba95fc79b729f2de19
Jack Harvey <53987140+Jack-harvey@users.noreply.github.com>
2024-12-23 15:17:48 +1100

    Add functions to create pixels on sketch-pad

--- script.js
+++ script.js
@@ -22,5 +22,43 @@ function getNewUserValue() {
 const configureButton = document.querySelector("#btnConfigure");
 
 configureButton.addEventListener("click", () => {
-  getNewUserValue();
+  numberOfDivsOnEachAxis = getNewUserValue();
 });
+
+const sketchPad = document.querySelector("#sketchPad");
+let numberOfDivsOnEachAxis = 16;
+
+document.addEventListener("DOMContentLoaded", () => {
+  createNewSketchPad();
+});
+
+function createNewSketchPad() {
+  removeAllChildNodes(sketchPad);
+  createDivsOnYAxis(numberOfDivsOnEachAxis);
+  createDivsOnXAxis(numberOfDivsOnEachAxis);
+}
+
+function createDivsOnXAxis(pixels) {
+  let allDivRows = [...document.querySelectorAll(".pixel-row")];
+  for (row of allDivRows) {
+    for (i = 0; i < pixels; i++) {
+      const newPixel = document.createElement("div");
+      newPixel.classList.toggle("pixel");
+      row.appendChild(newPixel);
+    }
+  }
+}
+
+function createDivsOnYAxis(pixels) {
+  for (i = 0; i < pixels; i++) {
+    const newRow = document.createElement("div");
+    newRow.classList.toggle("pixel-row");
+    sketchPad.appendChild(newRow);
+  }
+}
+
+function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+}
